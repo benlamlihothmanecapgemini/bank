e4148c94df497f8e06aa03c213a75e3727167cf2
ASRI <ayoub.asri@capgemini.com>
2022-01-14 18:11:34 +0100

    creation compte repository courant/epargne

--- src/main/java/com/demo/proxy/bank/dao/CompteRepository.java
+++ src/main/java/com/demo/proxy/bank/dao/CompteRepository.java
@@ -2,6 +2,8 @@ package com.demo.proxy.bank.dao;
 
 import com.demo.proxy.bank.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompteRepository extends JpaRepository<Compte, Long> {
 }
